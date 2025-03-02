a1774b0c04dbc1c0de84f22ad6e8f1a397e4f2e4
betoalmeida83 <roberto.almeida5@fatec.sp.gov.br>
2025-03-01 22:17:25 -0300

    Initial Commit

---
+++ src/main/java/entities/BusinessAccount.java
@@ -0,0 +1,35 @@
+package entities;
+
+public class BusinessAccount extends Account {
+
+    private Double loanLimit;
+
+    // CONSTRUCTORS
+
+    public BusinessAccount() {
+    }
+
+    public BusinessAccount(Integer number, String holder, Double balance, Double loanLimit) {
+        super(number, holder, balance);
+        this.loanLimit = loanLimit;
+    }
+
+    // GETTERS AND SETTERS
+
+
+    public Double getLoanLimit() {
+        return loanLimit;
+    }
+
+    public void setLoanLimit(Double loanLimit) {
+        this.loanLimit = loanLimit;
+    }
+
+    // METHODS
+
+    public void loan(Double amount) {
+        if(amount <= loanLimit){
+            deposit(amount);
+        }
+    }
+}
